969fa34f9d4443bdc61bd4c1e9de63cc6d625860
Denys <oktilon@gmail.com>
2026-06-02 22:18:04 +0200

    again spi1

--- lib/pn532_rpi.c
+++ lib/pn532_rpi.c
@@ -47,7 +47,7 @@
 #define _SPI_DATAWRITE                  (0x01)
 #define _SPI_DATAREAD                   (0x03)
 #define _SPI_READY                      (0x01)
-#define _SPI_CHANNEL                    (0)
+#define _SPI_CHANNEL                    (1)
 #define _NSS_PIN                        (4)
 
 #define _I2C_READY                      (0x01)
