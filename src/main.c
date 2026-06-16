505e051138300c0812c2e9fa8dabecb931fdaa4c
Denys <oktilon@gmail.com>
2026-06-16 14:16:07 +0200

    calc MAD crc8

--- src/main.c
+++ src/main.c
@@ -76,6 +76,26 @@ typedef struct access_bits {
   ((byte) & 0x02 ? '1' : '0'), \
   ((byte) & 0x01 ? '1' : '0') 
 
+
+uint8_t crc8_lookup[] = {
+    0x00, 0x1D, 0x3A, 0x27, 0x74, 0x69, 0x4E, 0x53,   0xE8, 0xF5, 0xD2, 0xCF, 0x9C, 0x81, 0xA6, 0xBB,
+    0xCD, 0xD0, 0xF7, 0xEA, 0xB9, 0xA4, 0x83, 0x9E,   0x25, 0x38, 0x1F, 0x02, 0x51, 0x4C, 0x6B, 0x76,
+    0x87, 0x9A, 0xBD, 0xA0, 0xF3, 0xEE, 0xC9, 0xD4,   0x6F, 0x72, 0x55, 0x48, 0x1B, 0x06, 0x21, 0x3C,
+    0x4A, 0x57, 0x70, 0x6D, 0x3E, 0x23, 0x04, 0x19,   0xA2, 0xBF, 0x98, 0x85, 0xD6, 0xCB, 0xEC, 0xF1,
+    0x13, 0x0E, 0x29, 0x34, 0x67, 0x7A, 0x5D, 0x40,   0xFB, 0xE6, 0xC1, 0xDC, 0x8F, 0x92, 0xB5, 0xA8,
+    0xDE, 0xC3, 0xE4, 0xF9, 0xAA, 0xB7, 0x90, 0x8D,   0x36, 0x2B, 0x0C, 0x11, 0x42, 0x5F, 0x78, 0x65,
+    0x94, 0x89, 0xAE, 0xB3, 0xE0, 0xFD, 0xDA, 0xC7,   0x7C, 0x61, 0x46, 0x5B, 0x08, 0x15, 0x32, 0x2F,
+    0x59, 0x44, 0x63, 0x7E, 0x2D, 0x30, 0x17, 0x0A,   0xB1, 0xAC, 0x8B, 0x96, 0xC5, 0xD8, 0xFF, 0xE2,
+    0x26, 0x3B, 0x1C, 0x01, 0x52, 0x4F, 0x68, 0x75,   0xCE, 0xD3, 0xF4, 0xE9, 0xBA, 0xA7, 0x80, 0x9D,
+    0xEB, 0xF6, 0xD1, 0xCC, 0x9F, 0x82, 0xA5, 0xB8,   0x03, 0x1E, 0x39, 0x24, 0x77, 0x6A, 0x4D, 0x50,
+    0xA1, 0xBC, 0x9B, 0x86, 0xD5, 0xC8, 0xEF, 0xF2,   0x49, 0x54, 0x73, 0x6E, 0x3D, 0x20, 0x07, 0x1A,
+    0x6C, 0x71, 0x56, 0x4B, 0x18, 0x05, 0x22, 0x3F,   0x84, 0x99, 0xBE, 0xA3, 0xF0, 0xED, 0xCA, 0xD7,
+    0x35, 0x28, 0x0F, 0x12, 0x41, 0x5C, 0x7B, 0x66,   0xDD, 0xC0, 0xE7, 0xFA, 0xA9, 0xB4, 0x93, 0x8E,
+    0xF8, 0xE5, 0xC2, 0xDF, 0x8C, 0x91, 0xB6, 0xAB,   0x10, 0x0D, 0x2A, 0x37, 0x64, 0x79, 0x5E, 0x43,
+    0xB2, 0xAF, 0x88, 0x95, 0xC6, 0xDB, 0xFC, 0xE1,   0x5A, 0x47, 0x60, 0x7D, 0x2E, 0x33, 0x14, 0x09,
+    0x7F, 0x62, 0x45, 0x58, 0x0B, 0x16, 0x31, 0x2C,   0x97, 0x8A, 0xAD, 0xB0, 0xE3, 0xFE, 0xD9, 0xC4
+};
+
 typedef struct block_data {
     int block;
     uint8_t data[16];
@@ -93,9 +113,10 @@ int       gKeyCount       = 0;
 BlockData *gWriteData     = NULL;
 uint8_t   gWriteCount     = 0;
 int       gReadMADKey     = -1;
+BlockData gMADData[4]     = {0};
 
 // Long command line options
-const struct option longOptions[] = { // vqxk:r:w:h
+const struct option longOptions[] = { // hvqxk:r:w:m::
     {"help",     no_argument,       0, 'h'},
     {"verbose",  no_argument,       0, 'v'},
     {"quiet",    no_argument,       0, 'q'},
@@ -103,7 +124,8 @@ const struct option longOptions[] = { // vqxk:r:w:h
     {"key",      required_argument, 0, 'k'},
     {"read",     required_argument, 0, 'r'},
     {"write",    required_argument, 0, 'w'},
-    {"mad",      optional_argument, 0, 'm'}
+    {"mad",      optional_argument, 0, 'm'},
+    {NULL, 0, 0, '\0'}
 };
 
 const char *helpOptions[] = {
@@ -116,6 +138,7 @@ const char *helpOptions[] = {
     , "Data to write in hex format (16 bytes) prefixed by block number\n"
       "\tseparated by : (e.g. 1:00112233445566778899AABBCCDDEEFF)\n"
       "\tTrailing bytes will be ignored, missing bytes will be filled with 0"
+    , "Read MIFARE Application Directory (MAD) list"
 };
 
 const char *logLevelHeaders[] = {
@@ -323,6 +346,22 @@ int dumpAccessBits(AccessBits *ab, uint8_t blk) {
     return 0;
 }
 
+void dumpMADSector() {
+    uint8_t ix, ib;
+    uint8_t crc = 0xc7; // MAD CRC8 Init value
+    // CRC8
+    ib = 1;
+    for (ix = 1; ix < 32; ix++) {
+        if(ix > 15) ib = 2;
+        crc = crc8_lookup[crc ^ gMADData[ib].data[ix - (ib - 1) * 16]];
+    }
+    log_all("CRC8 Calc=%02hhX, Read=%02hhX", crc, gMADData[1].data[0]);
+    // Sector 1
+    for (ix = 2; ix < 16; ix++) {
+        //
+    }
+}
+
 void parseWriteData (const char *list) {
     char *p = strchr (list, ':');
     if (!p) {
@@ -476,6 +515,8 @@ int addKey(char *arg) {
     char bByte[] = { 0, 0, 0 };
     Key key;
 
+    if (!arg) return -1;
+
     memset (&key, 0, sizeof(key));
     s = strlen(arg);
     if (s > 12) {
@@ -511,7 +552,8 @@ int parseArguments (int argc, char **argv) {
     int i, iExit = 0;
     
 
-    while ((i = getopt_long (argc, argv, "vqxk:r:w:hm::", longOptions, NULL)) != -1) {
+    while ((i = getopt_long (argc, argv, "hvqxk:r:w:m::", longOptions, NULL)) != -1) {
+        printf("Opt: %d [%c], ix:%d, optarg:%s\n", i, (char)i, optind, optarg ? optarg : "NULL");
         switch (i) {
             case 'v': // verbose
                 gLogLevel++;
@@ -542,8 +584,11 @@ int parseArguments (int argc, char **argv) {
                 break;
 
             case 'm': // MAD (MiFare Application Directory)
-                printf("MAD key: %s\n", optarg);
-                gReadMADKey = addKey(optarg);
+                if (optarg) {
+                    gReadMADKey = addKey(optarg);
+                } else {
+                    gReadMADKey = addKey("a0a1a2a3a4a5");
+                }
                 break;
 
             case 'k': // key
@@ -653,6 +698,31 @@ int readBlock(PN532 *pReader, uint8_t *uid, uint8_t uid_len, uint8_t block_numbe
     return PN532_ERROR_NONE;
 }
 
+int readMadBlock(PN532 *pReader, uint8_t *uid, uint8_t uid_len, uint8_t block_number) {
+    uint8_t pn532_error = PN532_ERROR_NONE;
+    uint8_t buff[255];
+
+    log_dbg ("Auth MAD sector by key[%d] %s...", gReadMADKey, dumpHexData(keys[gReadMADKey].key, 6, 0));
+    pn532_error = PN532_MifareClassicAuthenticateBlock(pReader, uid, uid_len,
+            block_number, MIFARE_CMD_AUTH_A, keys[gReadMADKey].key);
+
+    if (pn532_error != PN532_ERROR_NONE) {
+        log_wrn ("Auth block %hhu error 0x%hhX (%s)", block_number, pn532_error, pn532_errorstr(pn532_error));
+        return -1;
+    }
+
+    pn532_error = PN532_MifareClassicReadBlock(pReader, buff, block_number);
+    if (pn532_error != PN532_ERROR_NONE) {
+        log_wrn ("Read block %hhu error 0x%X (%s)", block_number, pn532_error, pn532_errorstr(pn532_error));
+        return -1;
+    }
+
+    gMADData[block_number].block = block_number;
+    memcpy(gMADData[block_number].data, buff, 16);
+
+    return 0;
+}
+
 int main(int argc, char** argv) {
     uint8_t buff[255], doLoop = 1, block_number;
     uint8_t uid[MIFARE_UID_MAX_LENGTH];
@@ -667,7 +737,7 @@ int main(int argc, char** argv) {
         memcpy(&(keys[0]), &defaultKey, sizeof(Key));
         gKeyCount++;
     }
-    if (!gBlocks && gWriteCount == 0) {
+    if (!gBlocks && gWriteCount == 0 && gReadMADKey < 0) {
         log_err ("No blocks specified for read or write, use -r or -w options");
         return -1;
     }
@@ -707,7 +777,7 @@ int main(int argc, char** argv) {
             }
             doLoop = 0;
         }
-    } else {
+    } else if (gBlocksCnt > 0) {
         while (doLoop) {
             log_all ("Scan your RFID/NFC card...");
             memset (uid, 0, MIFARE_UID_MAX_LENGTH);
@@ -729,6 +799,34 @@ int main(int argc, char** argv) {
             }
             sleep(3);
         }
+    } else if (gReadMADKey >= 0) {
+        while (doLoop) {
+            log_all ("Read MAD from RFID/NFC card...");
+            memset (uid, 0, MIFARE_UID_MAX_LENGTH);
+            while (doLoop) {
+                // Check if a card is available to read
+                gAuthSector = -1;
+                uid_len = PN532_ReadPassiveTarget(&pn532, uid, PN532_MIFARE_ISO14443A, 1000);
+                if (uid_len != PN532_STATUS_ERROR) {
+                    log_all ("Found card with UID: \033[96m%s\033[0m", dumpHexData(uid, uid_len, 0));
+                    break;
+                }
+            }
+            if (!doLoop) break;
+            log_inf ("Reading MAD blocks...");
+            for (block_number = 0; block_number < 4; block_number ++) {
+                if (readMadBlock (&pn532, uid, uid_len, block_number) < 0) {
+                    doLoop = 0;
+                    break;
+                }
+            }
+            if (doLoop) {
+                dumpMADSector();
+                doLoop = 0;
+            }
+        }
+    } else {
+        log_all ("Nothing to do");
     }
 
     return 0;
